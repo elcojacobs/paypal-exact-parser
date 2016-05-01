7c58f6171364985f26e4facdadb2115b45cc665f
Elco Jacobs <elco@brewpi.com>
2016-05-01 21:15:39 +0200

    Changed to run on python 3

--- paypal-parse.py
+++ paypal-parse.py
@@ -1,10 +1,13 @@
-import unicodecsv as csv
-from pprint import pprint
+import csv
 import locale
 #locale._print_locale()
-locale.setlocale(locale.LC_ALL, 'Dutch')
+
+#locale.setlocale(locale.LC_ALL, 'nl_NL')
+
 #locale.setlocale(locale.LC_ALL, '')
-#locale._print_locale()
+locale.setlocale(locale.LC_ALL, 'Dutch')
+#print(locale.getdefaultlocale(locale.LC_ALL))
+#print(locale.locale_alias)
 
 # gb_bank = 1110
 gb_kruis = 1292
@@ -13,13 +16,14 @@ gb_kosten = 5561
 
 omrekeningen = {}
 
-input = 'PayPal transacties 2015-09.csv'
+input = 'PayPal 2016-01.csv'
 output = input.rstrip(".csv") + "-exact-import.csv"
 
 with open(input) as csvfile:
-    with open(output, 'wb') as output:
+    with open(output, 'w', newline='') as output:
         writer = csv.writer(output, delimiter=',',quoting=csv.QUOTE_ALL)
-        header = [h.lstrip() for h in csvfile.next().split(',')]
+        headerline = csvfile.readline()
+        header = [h.lstrip() for h in headerline.split(',')]
         reader = csv.DictReader(csvfile, fieldnames=header)
         for row in reader:
             factuur_nummer = row['Factuurnummer']
@@ -39,7 +43,7 @@ with open(input) as csvfile:
                 bruto2 = None
                 reader2 = reader
                 if fee != 0:
-                    print "ERROR: vreemde valuta met fee niet nul"
+                    print("ERROR: vreemde valuta met fee niet nul")
                     exit(1)
                 for row2 in reader2:
                     if row['Transactiereferentie'] == row2['Ref.-id transactie'] and row2['Valuta'] == 'EUR':
@@ -47,14 +51,15 @@ with open(input) as csvfile:
                         bruto2 = locale.atof(row2['Bruto'])
                         fee2 = locale.atof(row2['Kosten'])
                         if fee2 != 0:
-                            print "ERROR: vreemde valuta met fee niet nul"
+                            print("ERROR: vreemde valuta met fee niet nul")
                             exit(1)
                         break
-                omschrijving = omschrijving + " ({0} {1})".format(bruto, row['Valuta'])
+                origineel_bedrag = " ({0} {1})".format(bruto, row['Valuta'])
+                omschrijving = omschrijving[:(60-len(origineel_bedrag))] + origineel_bedrag
                 bruto = bruto2
                 common = [row['Datum'], opmerking , omschrijving]
                 if bruto2 is None:
-                    print "Probleem met row1 " + row + "en row2 " + row2
+                    print("Probleem met row1 " + row + "en row2 " + row2)
                     continue
 
 
