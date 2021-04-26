4e3076b617506505640a235d6594403d35a20473
Elco Jacobs <elco@brewpi.com>
2021-04-26 14:29:15 +0200

    formatting

--- paypal-parse.py
+++ paypal-parse.py
@@ -1,11 +1,11 @@
 import csv
 #locale.setlocale(locale.LC_ALL, 'nl_NL')
 #locale.setlocale(locale.LC_ALL, '')
-#print(locale.getdefaultlocale(locale.LC_ALL))
-#print(locale.locale_alias)
+# print(locale.getdefaultlocale(locale.LC_ALL))
+# print(locale.locale_alias)
 import time
 
-#locale._print_locale()
+# locale._print_locale()
 
 
 # gb_bank = 1110
@@ -35,55 +35,55 @@ with open(previous_authorizations, encoding='utf-8-sig') as previous_csv:
     reader = csv.DictReader(previous_csv)
     for row in reader:
         for key, value in row.items():
-            row[key] = value.encode('iso-8859-1', 'ignore').decode('iso-8859-1') # replace characters that are not available in cp1252
+            row[key] = value.encode('iso-8859-1', 'ignore').decode('iso-8859-1')  # replace characters that are not available in cp1252
 
         code = row["Transaction Event Code"]
         factuur_nummer = row['Invoice Number']
         referentie = row['Transaction ID']
 
-        if code == 'T1300' or code == 'T1301': # authorization or re-authorization
+        if code == 'T1300' or code == 'T1301':  # authorization or re-authorization
             authorized[referentie] = factuur_nummer
 
 # after parsing, in pycharm: file->file enconding->windows 1252->convert
 with open(input, encoding='utf-8-sig') as csvfile:
     with open(output, 'w', newline='', encoding='utf-8-sig') as output:
-        writer = csv.writer(output, delimiter=',',quoting=csv.QUOTE_ALL)
-#       header = csv.reader(csvfile).next()        
+        writer = csv.writer(output, delimiter=',', quoting=csv.QUOTE_ALL)
+#       header = csv.reader(csvfile).next()
 #        header = [h.lstrip() for h in headerline.split(',')]
 #        header = urllib.unquote(header)
         reader = csv.DictReader(csvfile)
         for row in reader:
             for key, value in row.items():
-                row[key] = value.encode('iso-8859-1', 'ignore').decode('iso-8859-1') # replace characters that are not available in cp1252
+                row[key] = value.encode('iso-8859-1', 'ignore').decode('iso-8859-1')  # replace characters that are not available in cp1252
 
             factuur_nummer = row['Invoice Number']
             referentie = row['Transaction ID']
             kruisreferentie = row['Reference Txn ID']
-            bruto = float(row['Gross'].replace(thousands_sep,"").replace(dec_sep,"."))
-            fee = float(row['Fee'].replace(thousands_sep,"").replace(dec_sep,"."))
-            netto = float(row['Net'].replace(thousands_sep,"").replace(dec_sep,"."))
+            bruto = float(row['Gross'].replace(thousands_sep, "").replace(dec_sep, "."))
+            fee = float(row['Fee'].replace(thousands_sep, "").replace(dec_sep, "."))
+            netto = float(row['Net'].replace(thousands_sep, "").replace(dec_sep, "."))
             code = row["Transaction Event Code"]
             date = time.strptime(row['Date'], '%d/%m/%Y')
             datestr = time.strftime("%d-%m-%Y", date)
 
             print(code)
-            if code == 'T1300' or code == 'T1301': # authorization or re-authorization
+            if code == 'T1300' or code == 'T1301':  # authorization or re-authorization
                 authorized[referentie] = factuur_nummer
 
-            if code == 'T0006' and bruto > 0: # express checkout API of sale
+            if code == 'T0006' and bruto > 0:  # express checkout API of sale
                 if not factuur_nummer:
-                    if kruisreferentie: # recover invoice number from authorization
+                    if kruisreferentie:  # recover invoice number from authorization
                         print(row)
                         factuur_nummer = authorized[kruisreferentie]
 
             if row['Balance Impact'] == 'Memo':
-                continue # skip transactions not affecting balance
+                continue  # skip transactions not affecting balance
 
             omschrijving = row['Name'] + ' ' + factuur_nummer + ' ' + row['Type']
             omschrijving = omschrijving[:60]
             opmerking = referentie + ' ' + omschrijving
             opmerking = opmerking[:60]
-            common = [datestr, opmerking , omschrijving]
+            common = [datestr, opmerking, omschrijving]
 
             if row['Currency'] != 'EUR':
                 bruto2 = None
@@ -102,8 +102,8 @@ with open(input, encoding='utf-8-sig') as csvfile:
                     if (row['Transaction ID'] == row2['Reference Txn ID'] or row['Reference Txn ID'] == row2['Reference Txn ID']) \
                             and row2['Currency'] == 'EUR' and row['Time'] == row2['Time']:
                         # euro bedrag gevonden
-                        bruto2 = float(row2['Gross'].replace(thousands_sep,"").replace(dec_sep,"."))
-                        fee2 = float(row2['Fee'].replace(thousands_sep,"").replace(dec_sep,"."))
+                        bruto2 = float(row2['Gross'].replace(thousands_sep, "").replace(dec_sep, "."))
+                        fee2 = float(row2['Fee'].replace(thousands_sep, "").replace(dec_sep, "."))
                         print("Vreemde valuta conversie: ", common, bruto, bruto2, fee, fee2)
 
                         if '%.2f' % fee2 != "0.00":
@@ -115,7 +115,7 @@ with open(input, encoding='utf-8-sig') as csvfile:
                 fee_eur = fee / bruto * bruto2
                 bruto = bruto2
                 netto = bruto2 - fee_eur
-                common = [datestr, opmerking , omschrijving]
+                common = [datestr, opmerking, omschrijving]
                 if bruto2 is None:
                     print("Probleem met row1 " + str(row) + "en row2 " + str(row2))
 
